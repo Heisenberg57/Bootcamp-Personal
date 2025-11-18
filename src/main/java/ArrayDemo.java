8f7226b80243d0d209066de070ab3f7ead64e79b
Suyog Nimbre <heisenbergnimbre@gmail.com>
2025-11-18 21:33:39 +0530

    conditional logic 6

---
+++ src/main/java/ArrayDemo.java
@@ -0,0 +1,9 @@
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] customerIds = {101,102,103,104,105};
+
+        for (int i = 0; i<customerIds.length;i++){
+            System.out.println("Customer ID: "+customerIds[i]);
+        }
+    }
+}
