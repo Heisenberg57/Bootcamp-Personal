f86a9c1fd8831db26fce806176c38b9d0be37c36
Suyog Nimbre <heisenbergnimbre@gmail.com>
2025-11-19 22:44:18 +0530

    conditional logic 12

--- src/main/java/CustomerStatusChecker.java
+++ src/main/java/CustomerStatusChecker.java
@@ -1,7 +1,7 @@
 public class CustomerStatusChecker {
     public static void main(String[] args) {
-        int[] customerIds = {101, 205, 309, 412, 578};
-        String[] statuses = {"Active", "Inactive", "KYC Pending", "Blocked", "Premium"};
+        int[] customerIds = {101, 205, 309, 412, 578,450};
+        String[] statuses = {"Active", "Inactive", "KYC Pending", "Blocked", "Premium","poweruser"};
 
         int target = 412;
         boolean found = false;
