0ca2b7fcae946c3a4c5720c7f7a6c3773e59ff9f
Suyog Nimbre <heisenbergnimbre@gmail.com>
2025-11-24 18:04:18 +0530

    methods 2

---
+++ src/main/java/MethodParameters.java
@@ -0,0 +1,12 @@
+public class MethodParameters {
+
+    static void greetUser(String name){
+        System.out.println(" Hello "+ name + ", welcome");
+
+    }
+
+    public static void main(String[] args) {
+        greetUser("Suyog");
+        greetUser("Valentina");
+    }
+}
