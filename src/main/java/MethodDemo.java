1045ed5882e67dbd9bdfa1f79351c843d7bf81fb
Suyog Nimbre <heisenbergnimbre@gmail.com>
2025-11-24 17:48:41 +0530

    methods

---
+++ src/main/java/MethodDemo.java
@@ -0,0 +1,11 @@
+public class MethodDemo {
+    //simple method
+
+    static void greetUser(){
+        System.out.println("Hello Automation Engineer, Suyog");
+    }
+
+    public static void main(String[] args) {
+        greetUser();
+    }
+}
