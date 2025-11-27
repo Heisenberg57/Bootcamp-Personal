791246744849ea33e5f8f322f400cda71150344b
Suyog Nimbre <heisenbergnimbre@gmail.com>
2025-11-27 19:52:24 +0530

    Class 2

---
+++ src/main/java/CarTest.java
@@ -0,0 +1,14 @@
+public class CarTest {
+    public static void main(String[] args) {
+        Car c1 = new Car();
+        c1.brand = "BMW";
+        c1.year = 2021;
+        c1.start();
+
+        Car c2 = new Car();
+        c2.brand = "Audi";
+        c2.year=2022;
+        c2.start();
+
+    }
+}
