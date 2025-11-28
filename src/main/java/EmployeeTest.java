02286982f265b09ef4b246015e1c8e5a0eb64ad1
Suyog Nimbre <heisenbergnimbre@gmail.com>
2025-11-28 21:19:22 +0530

    inheritance 2

---
+++ src/main/java/EmployeeTest.java
@@ -0,0 +1,11 @@
+public class EmployeeTest {
+    public static void main(String[] args) {
+        Employee emp = new Employee();
+        emp.name = "Suyog";
+        emp.age = 24;
+        emp.salary = 80000;
+
+        emp.showEmployee();
+
+    }
+}
