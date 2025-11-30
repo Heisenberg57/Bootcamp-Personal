3888e4817ff2762c4dc8511adc1adc0012fb6627
Suyog Nimbre <heisenbergnimbre@gmail.com>
2025-11-30 17:20:28 +0530

    super

---
+++ src/main/java/User.java
@@ -0,0 +1,9 @@
+public class User {
+    User(){
+        System.out.println("User constructor called");
+    }
+
+    void display(){
+        System.out.println("This is User display method");
+    }
+}
