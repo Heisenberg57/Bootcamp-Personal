49c3d7ff5ff259287b1ff870af4ba456b64931cc
Suyog Nimbre <heisenbergnimbre@gmail.com>
2025-12-03 22:20:23 +0530

    arraylist

---
+++ src/main/java/ArrayListDemo.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Suyog");
+        names.add("Valentina");
+        names.add("Radha");
+        names.add("Nikhila");
+
+        System.out.println(names);
+    }
+}
