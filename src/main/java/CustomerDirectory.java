a10afc69fe0ae5c35fdbf542d26d9e245ed0eef9
Suyog Nimbre <heisenbergnimbre@gmail.com>
2025-12-04 16:55:24 +0530

    hashmap 3

---
+++ src/main/java/CustomerDirectory.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class CustomerDirectory {
+    public static void main(String[] args) {
+
+        HashMap<Integer,String> customers = new HashMap<>();
+
+        customers.put(101,"Suyog");
+        customers.put(202,"Valentina");
+        customers.put(303, "Priya");
+        customers.put(404, "Jordan");
+
+        System.out.println("=== Customer Directory ===");
+        System.out.println(customers);
+
+    }
+}
