a5a14f0afc74f9bed4c245a71748a73779aa61a8
Suyog Nimbre <heisenbergnimbre@gmail.com>
2025-12-08 18:37:18 +0530

    Add ResultUtils class for student result evaluation
    Implement ResultUtils class with getResult method to determine pass/fail based on marks.

---
+++ src/main/java/ResultUtils.java
@@ -0,0 +1,32 @@
+package JavaTest;
+
+public class ResultUtils {
+	
+	public static String getResult(int marks) {
+		
+		if(marks>=40) {
+			return "PASS";
+		}
+		else {
+			return "FAIL";
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int student1 = 85;
+		int student2 = 33;
+		
+		String result1 =  getResult(student1);
+		String result2 = getResult(student2);
+		
+		System.out.println("Student 1: " + result1);
+        System.out.println("Student 2: " + result2);
+		
+		
+		
+
+	}
+
+}
