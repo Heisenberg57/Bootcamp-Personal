c276f1e6fd7d3a6cc2446d0722a95aaa3d9232ce
Suyog Nimbre <heisenbergnimbre@gmail.com>
2025-12-09 15:24:08 +0530

    Add TestRunner class for executing test cases

---
+++ src/main/java/TestRunner.java
@@ -0,0 +1,17 @@
+package JavaTest;
+
+public class TestRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestCase tc1 = new TestCase();
+		
+		//Set Fields
+		tc1.testName = "Login Test";
+		tc1.testDescription = "Verify user can login with valid credentials";
+		
+		tc1.run();
+
+	}
+
+}
