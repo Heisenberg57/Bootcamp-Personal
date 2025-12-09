ad84810e48bf5c5da1b2c30592e7d38d89fb2a84
Suyog Nimbre <heisenbergnimbre@gmail.com>
2025-12-09 15:48:59 +0530

    Enhance TestCase with steps and result matching
    Added steps and expectedResultMatch fields to TestCase class. Updated run method to reflect test execution status based on expected result match.

--- src/main/java/TestCase.java
+++ src/main/java/TestCase.java
@@ -5,14 +5,24 @@ public class TestCase {
 		//fields(attributes)
 		String testName;
 		String testDescription;
+		int steps;
+		boolean expectedResultMatch;
 		String status;
 		
 		//Method to execute the test.
 		public void run() {
 			System.out.println("Running test: "+ testName);
 			System.out.println("Description: "+testDescription);
+			System.out.println("Test Execution took these much steps: "+steps);
 			
-			status ="PASS";
+			if(expectedResultMatch) {
+				status ="PASS";
+			}
+			else {
+				status="FAIL";
+			}
+
+			
 			System.out.println("Status: "+status);
 		}
 		
