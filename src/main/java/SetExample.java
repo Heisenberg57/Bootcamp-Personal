8254a9bae135a122bb58afe0e0b32f02cdd22d31
Suyog Nimbre <heisenbergnimbre@gmail.com>
2025-12-25 21:59:00 +0530

    Set

---
+++ src/main/java/SetExample.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetExample {
+    public static void main(String[] args) {
+
+        Set<String> browsers = new HashSet<>();
+
+        browsers.add("Chrome");
+        browsers.add("Firefox");
+        browsers.add("Chrome"); // duplicate
+
+        System.out.println(browsers);
+    }
+}
