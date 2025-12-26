d266f2df0d266419c0bd67822fcf2eff8e6a36d2
Suyog Nimbre <heisenbergnimbre@gmail.com>
2025-12-26 17:36:48 +0530

    Comments2

--- src/main/java/MapExample.java
+++ src/main/java/MapExample.java
@@ -4,6 +4,7 @@ import java.util.Map;
 public class MapExample {
     public static void main(String[] args) {
 
+        // Map: use when lookup matters (e.g. test results)
         Map<String, String> testResults = new HashMap<>();
 
         testResults.put("Login Test", "PASS");
